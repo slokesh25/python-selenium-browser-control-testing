9fac1b8138c7e651c007b675a5213539644b63a6
lokesh <s.lokesh2545@gmail.com>
2026-04-15 12:45:18 +0530

    updated work.py with new features

--- work.py
+++ work.py
@@ -269,6 +269,36 @@
 # print("source",source)
 # driver.close()
 
+# method 1 for id selector 
+
+# from selenium.webdriver import Chrome,ChromeOptions
+# from time import sleep
+# opts=ChromeOptions()
+# opts.add_experimental_option("detach",True)
+# driver=Chrome(opts)
+# driver.get("https://www.amazon.in/")
+# driver.maximize_window()
+# sleep(3)
+# search_tf=driver.find_element("id","twotabsearchtextbox")
+# search_tf.send_keys("mobiles")
+# sleep(3)
+# search_btn=driver.find_element("id","nav-search-submit-button")
+# search_btn.click()
+
+# method 2 for id selector
+
+# from selenium.webdriver import Chrome,ChromeOptions
+# from time import sleep
+# opts=ChromeOptions()
+# opts.add_experimental_option("detach",True)
+# driver=Chrome(opts)
+# driver.get("https://www.amazon.in/")
+# driver.maximize_window()
+# sleep(3)
+# driver.find_element("id","twotabsearchtextbox").send_keys("mobiles")
+# sleep(2)
+# driver.find_element("id","nav-search-submit-button").click()
+
 
 
 
