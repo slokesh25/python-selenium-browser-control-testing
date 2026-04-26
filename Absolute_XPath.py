8dbc9157bb7d46aaacc286da362d37ede0083b4e
lokesh <s.lokesh2545@gmail.com>
2026-04-26 21:27:26 +0530

    absolute path

--- Absolute_XPath.py
+++ Absolute_XPath.py
@@ -35,3 +35,138 @@ sleep(3)
 
 driver.find_element("xpath","/html/body/div[4]/div[1]/div[4]/div[2]/form/div/div[2]/div[2]/div[2]/div[1]/div[1]/input").click()
 sleep(3)
+
+# no:2
+
+
+from selenium.webdriver import Chrome,ChromeOptions
+from time import sleep
+
+opts=ChromeOptions()
+opts.add_experimental_option("detach",True)
+
+driver=Chrome(opts)
+driver.get("https://www.instagram.com/")
+
+driver.maximize_window()
+sleep(3)
+
+driver.find_element("xpath","(//input[contains(@class,'x1i10hfl xggy1nq xtpw4lu')])[1]").send_keys("lokesh")
+sleep(3)
+
+driver.find_element("xpath","(//input[contains(@class,'x1i10hfl xggy1nq xtpw4lu')])[2]").send_keys('loki123')
+sleep(3)
+
+driver.find_element("xpath","(//span[.='Log in'])[1]").click()
+sleep(3)
+
+driver.close()
+
+# no:3
+
+from selenium.webdriver import Chrome,ChromeOptions
+from time import sleep
+
+opts=ChromeOptions()
+opts.add_experimental_option("detach",True)
+
+driver=Chrome(opts)
+driver.get("https://www.instagram.com/")
+
+driver.maximize_window()
+sleep(3)
+
+driver.find_element("xpath","//input[@id='_r_2_']").send_keys('lokesh')
+sleep(3)
+
+driver.find_element("xpath","//input[@type='password']").send_keys('loki123')
+sleep(3)
+
+driver.find_element("xpath","(//span[.='Log in'])[1]").click()
+sleep(3)
+
+driver.close()
+
+# no:4
+
+from selenium.webdriver import Chrome,ChromeOptions
+from time import sleep
+
+opts=ChromeOptions()
+opts.add_experimental_option("detach",True)
+
+driver=Chrome(opts)
+driver.get("https://demowebshop.tricentis.com/")
+
+driver.maximize_window()
+sleep(3)
+
+driver.find_element("xpath","//a[text()='Register']").click()
+sleep(3)
+
+driver.find_element("xpath","(//a[contains(text(),'Books')])[3]").click()
+sleep(3)
+
+driver.close()
+
+# no:5
+
+from selenium.webdriver import Chrome,ChromeOptions
+from time import sleep
+
+opts=ChromeOptions()
+opts.add_experimental_option("detach",True)
+
+driver=Chrome(opts)
+driver.get("https://www.facebook.com/")
+
+driver.maximize_window()
+sleep(3)
+
+driver.find_element("xpath","//input[@id='_R_oiqjbjb9pb6amH1_']").send_keys("lokesh")
+sleep(3)
+
+driver.find_element("xpath","//input[@type='password']").send_keys('123445567')
+sleep(3)
+
+driver.find_element("xpath","(//span[.='Log in'])[1]").click()
+sleep(3)
+
+# no:6
+
+
+from selenium.webdriver import Chrome,ChromeOptions
+from time import sleep
+
+opts=ChromeOptions()
+opts.add_experimental_option("detach",True)
+
+driver=Chrome(opts)
+driver.get("https://demowebshop.tricentis.com/")
+
+driver.maximize_window()
+sleep(3)
+
+driver.find_element("xpath","//a[@href='/register']").click()
+sleep(3)
+
+driver.find_element("xpath","(//input[@type='radio'])[1]").click()
+sleep(3)
+
+driver.find_element("xpath","(//input[@type='text'])[3]").send_keys('lokesh')
+sleep(3)
+
+driver.find_element("xpath","(//input[@type='text'])[4]").send_keys("sathiyam")
+sleep(3)
+
+driver.find_element("xpath","(//input[@type='text'])[5]").send_keys("lokesh@gmail.com")
+sleep(3)
+
+driver.find_element("xpath","(//input[@type='password'])[1]").send_keys("lokesh123")
+sleep(3)
+
+driver.find_element("xpath","(//input[@type='password'])[2]").send_keys("lokesh123")
+sleep(3)
+
+driver.find_element("xpath","//input[@value='Register']").click()
+sleep(3)
--- full_programs.py
+++ full_programs.py
@@ -469,6 +469,192 @@
 
 # xpath absolute
 
+# from selenium.webdriver import Chrome,ChromeOptions
+# from time import sleep
+
+# opts=ChromeOptions()
+# opts.add_experimental_option("detach",True)
+
+# driver=Chrome(opts)
+# driver.get("https://demowebshop.tricentis.com/")
+
+# driver.maximize_window()
+# sleep(3)
+
+# driver.find_element("xpath","/html/body/div[4]/div[1]/div[1]/div[2]/div[1]/ul/li[1]/a").click()
+# sleep(3)
+
+# # sleep(3)
+
+# driver.find_element("xpath","/html/body/div[4]/div[1]/div[4]/div[2]/form/div/div[2]/div[2]/div[2]/div[2]/input").send_keys("lokesh")
+# sleep(3)
+
+# driver.find_element("xpath","/html/body/div[4]/div[1]/div[4]/div[2]/form/div/div[2]/div[2]/div[2]/div[3]/input").send_keys("sathiyam")
+# sleep(3)
+
+# driver.find_element("xpath","/html/body/div[4]/div[1]/div[4]/div[2]/form/div/div[2]/div[2]/div[2]/div[4]/input").send_keys("risha@gmail.com")
+# sleep(3)
+
+# driver.find_element("xpath","/html/body/div[4]/div[1]/div[4]/div[2]/form/div/div[2]/div[3]/div[2]/div[1]/input").send_keys("loki123")
+# sleep(3)
+
+# driver.find_element("xpath","/html/body/div[4]/div[1]/div[4]/div[2]/form/div/div[2]/div[3]/div[2]/div[2]/input").send_keys("loki123")
+# sleep(3)
+
+# driver.find_element("xpath","/html/body/div[4]/div[1]/div[4]/div[2]/form/div/div[2]/div[4]/input").click()
+# sleep(3) 
+
+# driver.find_element("xpath","/html/body/div[4]/div[1]/div[4]/div[2]/form/div/div[2]/div[2]/div[2]/div[1]/div[1]/input").click()
+# sleep(3)
+
+
+# from selenium.webdriver import Chrome,ChromeOptions
+# from time import sleep
+
+# opts=ChromeOptions()
+# opts.add_experimental_option("detach",True)
+
+# driver=Chrome(opts)
+# driver.get("https://demowebshop.tricentis.com/")
+
+# driver.maximize_window()
+# sleep(3)
+
+# driver.find_element("xpath","//a[@href='/register']").click()
+# sleep(3)
+
+# driver.find_element("xpath","(//input[@type='radio'])[1]").click()
+# sleep(3)
+
+# driver.find_element("xpath","(//input[@type='text'])[3]").send_keys('lokesh')
+# sleep(3)
+
+# driver.find_element("xpath","(//input[@type='text'])[4]").send_keys("sathiyam")
+# sleep(3)
+
+# driver.find_element("xpath","(//input[@type='text'])[5]").send_keys("lokesh@gmail.com")
+# sleep(3)
+
+# driver.find_element("xpath","(//input[@type='password'])[1]").send_keys("lokesh123")
+# sleep(3)
+
+# driver.find_element("xpath","(//input[@type='password'])[2]").send_keys("lokesh123")
+# sleep(3)
+
+# driver.find_element("xpath","//input[@value='Register']").click()
+# sleep(3)
+
+
+# from selenium.webdriver import Chrome,ChromeOptions
+# from time import sleep
+
+# opts=ChromeOptions()
+# opts.add_experimental_option("detach",True)
+
+# driver=Chrome(opts)
+# driver.get("https://www.facebook.com/")
+
+# driver.maximize_window()
+# sleep(3)
+
+# driver.find_element("xpath","//input[@id='_R_oiqjbjb9pb6amH1_']").send_keys("lokesh")
+# sleep(3)
+
+# driver.find_element("xpath","//input[@type='password']").send_keys('123445567')
+# sleep(3)
+
+# driver.find_element("xpath","(//span[.='Log in'])[1]").click()
+# sleep(3)
+
+# from selenium.webdriver import Chrome,ChromeOptions
+# from time import sleep
+
+# opts=ChromeOptions()
+# opts.add_experimental_option("detach",True)
+
+# driver=Chrome(opts)
+# driver.get("https://demowebshop.tricentis.com/")
+
+# driver.maximize_window()
+# sleep(3)
+
+# driver.find_element("xpath","//a[text()='Register']").click()
+# sleep(3)
+
+# driver.find_element("xpath","(//a[contains(text(),'Books')])[3]").click()
+# sleep(3)
+
+# driver.close()
+
+
+# from selenium.webdriver import Chrome,ChromeOptions
+# from time import sleep
+
+# opts=ChromeOptions()
+# opts.add_experimental_option("detach",True)
+
+# driver=Chrome(opts)
+# driver.get("https://www.instagram.com/")
+
+# driver.maximize_window()
+# sleep(3)
+
+# driver.find_element("xpath","//input[@id='_r_2_']").send_keys('lokesh')
+# sleep(3)
+
+# driver.find_element("xpath","//input[@type='password']").send_keys('loki123')
+# sleep(3)
+
+# driver.find_element("xpath","(//span[.='Log in'])[1]").click()
+# sleep(3)
+
+# driver.close()
+
+# from selenium.webdriver import Chrome,ChromeOptions
+# from time import sleep
+
+# opts=ChromeOptions()
+# opts.add_experimental_option("detach",True)
+
+# driver=Chrome(opts)
+# driver.get("https://www.instagram.com/")
+
+# driver.maximize_window()
+# sleep(3)
+
+# driver.find_element("xpath","(//input[contains(@class,'x1i10hfl xggy1nq xtpw4lu')])[1]").send_keys("lokesh")
+# sleep(3)
+
+# driver.find_element("xpath","(//input[contains(@class,'x1i10hfl xggy1nq xtpw4lu')])[2]").send_keys('loki123')
+# sleep(3)
+
+# driver.find_element("xpath","(//span[.='Log in'])[1]").click()
+# sleep(3)
+
+# driver.close()
+
+
+# from selenium.webdriver import Chrome,ChromeOptions
+# from time import sleep
+
+# opts=ChromeOptions()
+# opts.add_experimental_option("detach",True)
+
+# driver=Chrome(opts)
+# driver.get("https://www.amazon.in/")
+
+# driver.maximize_window()
+# sleep(3)
+
+# driver.find_element("id","twotabsearchtextbox").send_keys('apple 17 pro max')
+# sleep(3)
+
+# driver.find_element("id","nav-search-submit-button").click()
+# sleep(3)
+
+# driver.find_element("xpath","//span[contains(text(),'iPhone 17 Pro Max 256 GB:')]").click()
+# sleep(3)
+
 from selenium.webdriver import Chrome,ChromeOptions
 from time import sleep
 
@@ -476,33 +662,21 @@ opts=ChromeOptions()
 opts.add_experimental_option("detach",True)
 
 driver=Chrome(opts)
-driver.get("https://demowebshop.tricentis.com/")
+driver.get("https://www.flipkart.com/")
 
 driver.maximize_window()
 sleep(3)
 
-driver.find_element("xpath","/html/body/div[4]/div[1]/div[1]/div[2]/div[1]/ul/li[1]/a").click()
+driver.find_element("xpath","//span[contains(@class,'b3wTlE')]").click()
 sleep(3)
 
+driver.find_element("class name","nw1UBF v1zwn2").send_keys('samsung s24 ultra')
+sleep(3)
+
+driver.find_element("")
+
+# driver.find_element("xpath","").send_keys('samsung s24 ultra')
 # sleep(3)
 
-driver.find_element("xpath","/html/body/div[4]/div[1]/div[4]/div[2]/form/div/div[2]/div[2]/div[2]/div[2]/input").send_keys("lokesh")
-sleep(3)
 
-driver.find_element("xpath","/html/body/div[4]/div[1]/div[4]/div[2]/form/div/div[2]/div[2]/div[2]/div[3]/input").send_keys("sathiyam")
-sleep(3)
 
-driver.find_element("xpath","/html/body/div[4]/div[1]/div[4]/div[2]/form/div/div[2]/div[2]/div[2]/div[4]/input").send_keys("risha@gmail.com")
-sleep(3)
-
-driver.find_element("xpath","/html/body/div[4]/div[1]/div[4]/div[2]/form/div/div[2]/div[3]/div[2]/div[1]/input").send_keys("loki123")
-sleep(3)
-
-driver.find_element("xpath","/html/body/div[4]/div[1]/div[4]/div[2]/form/div/div[2]/div[3]/div[2]/div[2]/input").send_keys("loki123")
-sleep(3)
-
-driver.find_element("xpath","/html/body/div[4]/div[1]/div[4]/div[2]/form/div/div[2]/div[4]/input").click()
-sleep(3) 
-
-driver.find_element("xpath","/html/body/div[4]/div[1]/div[4]/div[2]/form/div/div[2]/div[2]/div[2]/div[1]/div[1]/input").click()
-sleep(3)
